2c57c4b63b95903dfb2f85ecc6a2f17ad6faa093
crisscde <cristophercuautzo@gmail.com>
2025-03-20 09:39:57 -0600

    Add OrderItem Model

---
+++ ecommerce-api/src/main/java/com/revature/models/OrderItem.java
@@ -0,0 +1,54 @@
+package com.revature.models;
+
+public class OrderItem {
+    private int orderItemId;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private double price;
+
+    private static int orderItemIdCounter = 1;
+
+    public OrderItem(int orderId, int productId, int quantity, double price) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+
+        this.orderItemId = orderItemIdCounter;
+        orderItemIdCounter++;
+    }
+
+    public OrderItem(){}
+
+    public int getOrderItemId() { return orderItemId; }
+
+    public void setOrderItemId(int orderItemId) { this.orderItemId = orderItemId; }
+
+    public int getOrderId() { return orderId; }
+
+    public void setOrderId(int orderId) { this.orderId = orderId; }
+
+    public int getProductId() { return productId; }
+
+    public void setProductId(int productId) { this.productId = productId; }
+
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public double getPrice() { return price; }
+
+    public void setPrice(double price) { this.price = price; }
+
+    @Override
+    public String toString() {
+        return "OrderItem {" +
+                "orderItemId = " + orderItemId +
+                ", orderId" + orderId +
+                ", productId = " + productId +
+                ", quantity = " + quantity +
+                ", price = " + price +
+                "}";
+    }
+}
