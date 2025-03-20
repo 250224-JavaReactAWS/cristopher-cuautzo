ae8f4d1f549d44101e15470426452c6c69a6c32e
crisscde <cristophercuautzo@gmail.com>
2025-03-20 09:25:35 -0600

    Add Product Model

---
+++ ecommerce-api/src/main/java/com/revature/models/Product.java
@@ -0,0 +1,53 @@
+package com.revature.models;
+
+public class Product {
+    private int productId;
+    private String name;
+    private String description;
+    private Double price;
+    private int stock;
+
+    private static int productIdCounter = 1;
+    public Product(String name, String description, Double price, int stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+
+        this.productId = productIdCounter;
+        productIdCounter++;
+    }
+
+    public Product(){}
+
+    public int getProductId() { return productId; }
+
+    public void setProductId(int productId) { this.productId = productId; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public Double getPrice() { return price; }
+
+    public void setPrice(Double price) { this.price = price; }
+
+    public int getStock() { return stock; }
+
+    public void setStock(int stock) { this.stock = stock; }
+
+    @Override
+    public String toString() {
+        return "Product {" +
+                "productId = " + productId + "'" +
+                ", name = '" + name + "'" +
+                ", description = '" + description + "'" +
+                ", price = " + price +
+                ", stock = " + stock +
+                "}";
+    }
+}
