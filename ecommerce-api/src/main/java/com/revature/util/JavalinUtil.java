79f6fb52de0f8a722bd75f1c94e4340a6df843e8
crisscde <cristophercuautzo@gmail.com>
2025-03-23 18:52:48 -0600

    Add new paths for Product Managment
    Path: "/products"
    GET paths:
      - "/available" => Get the products available
      - "/{name}" => Get a product by the name
    POST paths:
      - "/new" => Add a new product
      - "{productId}" => Update product details
    DELETE paths:
      - "{productsId}" => Delete a product

--- ecommerce-api/src/main/java/com/revature/util/JavalinUtil.java
+++ ecommerce-api/src/main/java/com/revature/util/JavalinUtil.java
@@ -1,8 +1,12 @@
 package com.revature.util;
 
+import com.revature.controllers.ProductController;
 import com.revature.controllers.UserController;
+import com.revature.repos.ProductDAO;
+import com.revature.repos.ProductDAOImpl;
 import com.revature.repos.UserDAO;
 import com.revature.repos.UserDAOImpl;
+import com.revature.services.ProductService;
 import com.revature.services.UserService;
 import io.javalin.Javalin;
 
@@ -14,13 +18,24 @@ public class JavalinUtil {
         UserService userService = new UserService(userDAO);
         UserController userController = new UserController(userService);
 
+        ProductDAO productDAO = new ProductDAOImpl();
+        ProductService productService = new ProductService(productDAO);
+        ProductController productController = new ProductController(productService);
+
         return Javalin.create(config -> {
             config.router.apiBuilder(() -> {
                 path("/users", () -> {
                     post("/register", userController:: registerUserHandler);
                     post("/login", userController:: loginHandler);
-                    put("/{userId}", userController:: updatedHandler);
+                    put("/{userId}", userController:: updateUserHandler);
                     get("/orders/{userId}", userController:: getAllOrdersById);
+                });
+                path("/products", () -> {
+                    get("/available", productController:: getProductsAvailable);
+                    get("/{name}", productController:: getProductByName);
+                    post("/new", productController:: addProductHandler);
+                    post("/{productId}", productController:: updateProductHandler);
+                    delete("/{productId}", productController:: deleteProductHandler);
                 });
             });
         }).start(port);
